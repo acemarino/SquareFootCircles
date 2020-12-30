cf2b16fd8839c5b38966e37a67f62ad81a68cf5a
acema <ace.marino@gmail.com>
2020-12-30 13:50:19 -0500

    Update Main.java
    created sample data sets

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.lang.Math;
+
+
 public class Main{
     public static void main(String[] args){
+
+        ArrayList<Double>SquareFeet= new ArrayList<Double>();
+        ArrayList<Double>SqureRoots= new ArrayList<Double>();
         
+        SquareFeet.add(1000.00);
+        SquareFeet.add(2000.00);
+        SquareFeet.add(10000.00);
+        SquareFeet.add(5000.00);
+        SquareFeet.add(15000.00);
+
+        for(int i =0; i <SquareFeet.size();i++){
+            double SqRt= Math.sqrt(SquareFeet.get(i));
+            SqureRoots.add(SqRt);
+        }
+        
+
     }
 }
